aaf60ed4f1eb11ca072f5a04ec4a698e4125ceb5
Victor Zhang <victorzw895@gmail.com>
2019-09-23 00:28:43 +1000

    almost finished portfolio

--- js/main.js
+++ js/main.js
@@ -1,36 +1,134 @@
 $(document).ready(function() {
+  let activeLink = null;
+  let clickedLink = false;
+  // hover
+
+  // $(this).hasClass("active")
+  // ? null
+  // : ()
+
+  $("#nav ul li a").hover(
+    function() {
+      if ($(this).hasClass("active")) {
+        $(this).css({
+          color: "black",
+          cursor: "pointer"
+        });
+      } else {
+        $(this).css({
+          color: "rgb(255, 255, 255, 0.85)",
+          cursor: "pointer"
+        });
+      }
+    },
+    function() {
+      if ($(this).hasClass("active")) {
+        $(this).css({
+          color: "black",
+          cursor: "pointer"
+        });
+      } else {
+        $(this).css({
+          color: "#aaa"
+        });
+      }
+    }
+  );
+
   // $('nav ul li a')
   $currentLink = null;
+  let currentSection = null;
+  let navHeight = $("#nav ul").outerHeight(true);
 
-  $("body").scroll(function(e) {
-    console.log("scrolling");
-    console.log(e.target.scrollTop);
-  });
-
-  $("#nav ul li").on("click", "a", function(e) {
-    e.preventDefault();
-    // Animate NavBar Link
+  const animateLink = link => {
     if ($currentLink) {
       $currentLink.animate(
         { color: "#aaa", backgroundColor: "rgb(15, 15, 15)" },
         500
       );
+      $currentLink.removeClass("active");
     }
-    $currentLink = $(this);
-    $currentLink.animate({ color: "black", backgroundColor: "white" }, 500);
+    if (!link) {
+      $currentLink = null;
+    } else {
+      $currentLink = link;
+      $currentLink.addClass("active");
+      $currentLink.animate({ color: "black", backgroundColor: "white" }, 500);
+    }
+  };
 
-    // Animate Scroll
-    let navHeight = $("#nav ul").outerHeight(true);
-    let scrollTo = $($currentLink.attr("href")).offset().top;
+  $("body").scroll(function(e) {
+    if (clickedLink) {
+      return;
+    }
+    if (
+      e.target.scrollTop >=
+      $("#passions").position().top + $("body").scrollTop() - navHeight
+    ) {
+      if (currentSection !== "passions") {
+        currentSection = "passions";
+        animateLink($("a[href='#passions']"));
+      }
+    } else if (
+      e.target.scrollTop >=
+      $("#expSkills").position().top + $("body").scrollTop() - navHeight
+    ) {
+      if (currentSection !== "expSkills") {
+        currentSection = "expSkills";
+        animateLink($("a[href='#experience']"));
+      }
+    } else if (
+      e.target.scrollTop >=
+      $("#projects").position().top + $("body").scrollTop() - navHeight
+    ) {
+      if (currentSection !== "projects") {
+        currentSection = "projects";
+        animateLink($("a[href='#projects']"));
+      }
+    } else if (
+      e.target.scrollTop >=
+      $("#about-me").position().top + $("body").scrollTop() - navHeight
+    ) {
+      if (currentSection !== "about-me") {
+        currentSection = "about-me";
+        animateLink($("a[href='#about-me']"));
+      }
+    } else {
+      if (currentSection) {
+        currentSection = null;
+        animateLink(null);
+      }
+    }
+  });
+
+  $("#nav ul li").on("click", "a", function(e) {
+    e.preventDefault();
+
+    clickedLink = true;
+
+    animateLink($(this));
+
+    // if ($currentLink) {
+    //   $currentLink.animate(
+    //     { color: "#aaa", backgroundColor: "rgb(15, 15, 15)" },
+    //     500
+    //   );
+    // }
+    // $currentLink = $(this);
+    // $currentLink.animate({ color: "black", backgroundColor: "white" }, 500);
+
+    let scrollTo = $($(this).attr("href")).offset().top;
     let currentPosition = $("body").scrollTop();
-    console.log($(this));
     $("html, body").animate(
       {
-        scrollTop: scrollTo + currentPosition - navHeight
+        scrollTop: scrollTo + currentPosition - navHeight + 1
       },
       500,
       "linear"
     );
+    setTimeout(() => {
+      clickedLink = false;
+    }, 550);
   });
 
   let $featured = null;
